aa1d52f30f2d65e4ff7d06a3968ec0e010afb5fc
erjagdeep2307 <erjagdeep2307@gmail.com>
2026-09-22 23:36:09 +0530

    CSS file changed.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter, Playfair_Display } from 'next/font/google';
 import './globals.css';
+
 import { ThemeProvider } from '@/components/ThemeContext';
 
 const inter = Inter({
